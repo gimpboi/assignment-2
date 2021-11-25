28008fe6a5ee1fbb389c055587b10e244e443461
JoeMightyRat <94189353+JoeMightyRat@users.noreply.github.com>
2021-11-25 20:35:56 +0000

    Update wordgame.py

--- wordgame.py
+++ wordgame.py
@@ -91,15 +91,18 @@ def update_hand(hand, word):
 
 
 def is_valid_word(word, hand, word_list):
-    new_hand = hand.copy()
     if word in word_list:
         counter = 0
         for i in word:
-            if i in new_hand and new_hand[i] > 0:
+            if (i in hand) and (hand[i] > 0):
+                hand[i] = hand.get(i, 0) - 1
                 counter += 1
-                new_hand[i] = new_hand.get(i, 0) - 1
+            elif (i in VOWELS and "*" in hand) and (hand['*'] > 0):
+                hand['*'] = hand.get('*', 0) - 1
+                counter += 1
         if len(word) == counter:
             return True
+
         else:
             return False
     else:
